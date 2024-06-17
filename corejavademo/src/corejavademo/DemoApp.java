61f38008d08f61c1408becb307c7755e86835da3
HarshaVardhan <athaluri.harsha@gmail.com>
2024-06-17 21:45:06 +0530

    user class added

--- corejavademo/src/corejavademo/DemoApp.java
+++ corejavademo/src/corejavademo/DemoApp.java
@@ -4,5 +4,7 @@ public class DemoApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Demo!!!!");
+		User u = new User(); // Creating Object
+		u.displayDetails();
 	}
 }
---
+++ corejavademo/src/corejavademo/User.java
@@ -0,0 +1,13 @@
+package corejavademo;
+
+public class User {
+
+	int userid = 1;
+	String username = "demosuer";
+	String addr = "HYD";
+	
+	void displayDetails() {
+		System.out.println(userid+ " :: "+ username + ":: "+ addr);
+	}
+	
+}
